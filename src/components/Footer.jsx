d7890ba6a4175e34fab7d17602601fb750ac455e
Hassen Ahmed <hassan.creative@gmail.com>
2024-07-18 00:20:56 +0200

    adjust text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import { nesmaLogo } from "../constants/images";
 import { useTranslation } from "react-i18next";
 
 const Footer = () => {
-  const { t } = useTranslation(["translation"]);
+  const { t, i18n } = useTranslation(["translation"]);
+  const isRTL = i18n.dir() === "rtl";
   return (
     <footer className="p-4 bg-primary">
       <div className="wrapper max-w-[1290px] w-full m-auto flex flex-col sm:flex-row gap-3 justify-between items-center text-white text-sm">
@@ -13,8 +14,19 @@ const Footer = () => {
         </small>
         <div className="right">
           <div className="logo flex items-center justify-center gap-2">
-            <img className="w-12 " src={nesmaLogo} alt="" />
-            <small>{t("nesma")}</small>
+            <small className=" flex items-center gap-2">
+              {isRTL ? (
+                <>
+                  <span>{t("nesma")}</span>
+                  <img className="w-12" src={nesmaLogo} alt="" />
+                </>
+              ) : (
+                <>
+                  <img className="w-12" src={nesmaLogo} alt="" />
+                  <span>{t("nesma")}</span>
+                </>
+              )}
+            </small>
           </div>
         </div>
       </div>
